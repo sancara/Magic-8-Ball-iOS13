656b19c896e6a6ec86a76d913e06b893d0fcc74d
Sancara <santiagocaraballo@gmail.com>
2022-01-05 07:45:28 -0300

    adding func to button

--- Magic 8 Ball/ViewController.swift
+++ Magic 8 Ball/ViewController.swift
@@ -10,9 +10,14 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    let ballArray = [#imageLiteral(resourceName: "ball1.png"),#imageLiteral(resourceName: "ball2.png"),#imageLiteral(resourceName: "ball3.png"),#imageLiteral(resourceName: "ball4.png"),#imageLiteral(resourceName: "ball5.png")]
 
+    @IBOutlet weak var imageView: UIImageView!
+    
+    let ballArray = [UIImage(named: "ball1"),UIImage(named: "ball2"),UIImage(named: "ball3"),UIImage(named: "ball4"),UIImage(named: "ball5")]
 
-
+    @IBAction func askButtonPressed(_ sender: Any) {
+        imageView.image = ballArray[2]
+    }
+    
 }
 
