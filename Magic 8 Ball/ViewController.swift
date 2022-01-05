2a625fc8a49698b5a9631e7e449a9f3999414e1f
Sancara <santiagocaraballo@gmail.com>
2022-01-05 07:50:48 -0300

    finish eight ball

--- Magic 8 Ball/ViewController.swift
+++ Magic 8 Ball/ViewController.swift
@@ -16,7 +16,7 @@ class ViewController: UIViewController {
     let ballArray = [UIImage(named: "ball1"),UIImage(named: "ball2"),UIImage(named: "ball3"),UIImage(named: "ball4"),UIImage(named: "ball5")]
 
     @IBAction func askButtonPressed(_ sender: Any) {
-        imageView.image = ballArray[2]
+        imageView.image = ballArray[Int.random(in: 0...4)]
     }
     
 }
